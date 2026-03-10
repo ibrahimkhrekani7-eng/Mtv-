e9ac7d8891f5a9b08466529688ba98ee04da097e
ibrahimkhrekani7-eng <ibrahimkhrekani7@gmail.com>
2026-03-10 07:28:05 +0300

    Fix import path for firebase-config.js

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-import { db } from 'firebase-config.js';
+import { db } from './firebase-config.js';
 import { collection, onSnapshot, doc } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-firestore.js";
 
 const translations = {
